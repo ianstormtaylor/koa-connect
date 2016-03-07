8ee81931e3832b5c0b7ae78e6bc43dd85e920fb1
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-03-07 12:03:28 -0800

    fix for async connect middleware

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function koaConnect(connectMiddleware) {
-
   return function koaConnect(ctx, next) {
-    connectMiddleware(ctx.req, ctx.res, (err) => {
-      if (err) throw err;
-      next();
-    });
+    return new Promise(function (resolve, reject) {
+      connectMiddleware(ctx.req, ctx.res, (err) => {
+        if (err) reject(err);
+        resolve(next())
+      });
+    })
   };
-
 };
